31c43e02f3088f80e69e3541b141e2b6dd054448
matthiasarnold83 <mat-ar@web.de>
2014-08-12 18:20:59 +0200

    changes from assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Two functions. first function creates the matrix. 
+## In order to work it needs a function, which can be created with matrix(data,nrow,crow). The data need to be inserted with c()
 
-## Write a short comment describing this function
+## First function is creating the matrix and setting up the lexical scoping. 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        S <- NULL
+        set <- function(y){
+                x <<- y
+                S <<- NULL
+        }
+        get <- function() x
+        setSolve <- function(solve) S <<- solve
+        getSolve <- function() S
+        list(set = set, get = get,
+             setSolve = setSolve,
+             getSolve = getSolve)
 }
 
 
-## Write a short comment describing this function
+## Second function creates the inverse and delivers it back to construct from the first function. 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        S <- x$getSolve()
+        if(!is.null(S)){
+                message("getting cached data")
+                return(S)
+        }
+        data <- x$get()
+        S <- solve(data,...)
+        x$setSolve(S)
+        S
 }
